cafd57fdafa38d8b3549cf158333f78e90bad55f
gerard-iartesana <gerard@iartesana.es>
2026-06-13 18:16:50 +0200

    feat: add activity logging to admin-chat PWA, update service worker cache versions to clear browser cache

--- chat-sw.js
+++ chat-sw.js
@@ -1,5 +1,5 @@
 // Chat Service Worker - Network First + Push Notifications + Badge
-const CACHE_VERSION = 'chat-v12-badges';
+const CACHE_VERSION = 'chat-v13-badges';
 
 self.addEventListener('install', event => {
     self.skipWaiting();
